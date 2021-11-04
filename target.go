216f43e04e7d2d3b77226e9f72565814bb727c80
Shiwei Zhang <shizh@microsoft.com>
2021-11-04 17:21:50 +0800

    define target
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

---
+++ target.go
@@ -0,0 +1,67 @@
+package oras
+
+import (
+	"context"
+	"io"
+
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+// Descriptor describes the disposition of targeted content.
+type Descriptor = ocispec.Descriptor
+
+// Fetcher fetches content.
+type Fetcher interface {
+	// Fetch fetches the content identified by the descriptor.
+	Fetch(ctx context.Context, target Descriptor) (io.ReadCloser, error)
+}
+
+// Pusher pushes content.
+type Pusher interface {
+	// Push pushes the content, matching the expected descriptor.
+	// Reader is perferred to Writer so that the suitable buffer size can be
+	// chosen by the underlying implementation. Furthermore, the implementation
+	// can also do reflection on the Reader for more advanced I/O optimization.
+	Push(ctx context.Context, expected Descriptor, content io.Reader) error
+}
+
+// Storage represents a content-addressable storage (CAS) where contents are
+// accessed via Descriptors.
+// The storage is designed to handle blobs of large sizes.
+type Storage interface {
+	Fetcher
+	Pusher
+
+	// Exists returns true if the described content exists.
+	Exists(ctx context.Context, target Descriptor) (bool, error)
+}
+
+// Metadata provides tag indexing services.
+type Metadata interface {
+	// Resolve resolves a tag to a descriptor.
+	Resolve(ctx context.Context, tag string) (Descriptor, error)
+
+	// Tag tags a descriptor with a string.
+	Tag(ctx context.Context, desc Descriptor, tag string) error
+}
+
+// Target is a CAS with tags.
+type Target interface {
+	Storage
+	Metadata
+}
+
+// ParentFinder finds out the parent nodes of a given node of a directed acyclic
+// graph.
+// ParentFinder is an extension of Storage.
+type ParentFinder interface {
+	FindParent(ctx context.Context, node Descriptor) ([]Descriptor, error)
+}
+
+// TagPusher pushes content with a tag.
+// TagPusher is an extension of Target.
+type TagPusher interface {
+	// PushWithTag pushes the content with a tag, matching the expected
+	// descriptor.
+	PushWithTag(ctx context.Context, expected Descriptor, content io.Reader, tag string) error
+}
