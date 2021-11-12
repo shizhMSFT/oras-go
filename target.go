358af1031631275a220e92d97528b6b96fff0545
Shiwei Zhang <shizh@microsoft.com>
2021-11-12 19:38:31 +0800

    refactor dag package
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- target.go
+++ target.go
@@ -4,42 +4,13 @@ import (
 	"context"
 	"io"
 
-	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"oras.land/oras-go/dag"
 )
-
-// Descriptor describes the disposition of targeted content.
-type Descriptor = ocispec.Descriptor
-
-// Fetcher fetches content.
-type Fetcher interface {
-	// Fetch fetches the content identified by the descriptor.
-	Fetch(ctx context.Context, target Descriptor) (io.ReadCloser, error)
-}
-
-// Pusher pushes content.
-type Pusher interface {
-	// Push pushes the content, matching the expected descriptor.
-	// Reader is perferred to Writer so that the suitable buffer size can be
-	// chosen by the underlying implementation. Furthermore, the implementation
-	// can also do reflection on the Reader for more advanced I/O optimization.
-	Push(ctx context.Context, expected Descriptor, content io.Reader) error
-}
-
-// Storage represents a content-addressable storage (CAS) where contents are
-// accessed via Descriptors.
-// The storage is designed to handle blobs of large sizes.
-type Storage interface {
-	Fetcher
-	Pusher
-
-	// Exists returns true if the described content exists.
-	Exists(ctx context.Context, target Descriptor) (bool, error)
-}
 
 // Resolver resolves reference tags.
 type Resolver interface {
 	// Resolve resolves a reference to a descriptor.
-	Resolve(ctx context.Context, reference string) (Descriptor, error)
+	Resolve(ctx context.Context, reference string) (dag.Descriptor, error)
 }
 
 // TagResolver provides reference tag indexing services.
@@ -47,12 +18,12 @@ type TagResolver interface {
 	Resolver
 
 	// Tag tags a descriptor with a reference string.
-	Tag(ctx context.Context, desc Descriptor, reference string) error
+	Tag(ctx context.Context, desc dag.Descriptor, reference string) error
 }
 
 // Target is a CAS with tags.
 type Target interface {
-	Storage
+	dag.Storage
 	TagResolver
 }
 
@@ -60,7 +31,7 @@ type Target interface {
 // graph.
 // ParentFinder is an extension of Storage.
 type ParentFinder interface {
-	FindParent(ctx context.Context, node Descriptor) ([]Descriptor, error)
+	FindParent(ctx context.Context, node dag.Descriptor) ([]dag.Descriptor, error)
 }
 
 // TagPusher pushes content with a reference tag.
@@ -68,5 +39,5 @@ type ParentFinder interface {
 type TagPusher interface {
 	// PushWithTag pushes the content with a reference tag, matching the
 	// expected descriptor.
-	PushWithTag(ctx context.Context, expected Descriptor, content io.Reader, reference string) error
+	PushWithTag(ctx context.Context, expected dag.Descriptor, content io.Reader, reference string) error
 }
