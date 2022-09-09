ceb9b317912a13be1c156018a5e3855160c077c3
Shiwei Zhang <shizh@microsoft.com>
2022-09-09 23:09:49 +0800

    simplify
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- content/reader.go
+++ content/reader.go
@@ -20,7 +20,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/opencontainers/go-digest"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"oras.land/oras-go/v2/internal/ioutil"
 )
 
 var (
@@ -36,6 +38,66 @@ var (
 	// there exists trailing data unread when the read terminates.
 	ErrTrailingData = errors.New("trailing data")
 )
+
+type VerifyReader struct {
+	base     *io.LimitedReader
+	verifier digest.Verifier
+	verified bool
+	err      error
+}
+
+func (vr *VerifyReader) Read(p []byte) (n int, err error) {
+	if vr.err != nil {
+		return 0, vr.err
+	}
+
+	n, err = vr.base.Read(p)
+	if err != nil {
+		if err == io.EOF && vr.base.N > 0 {
+			err = io.ErrUnexpectedEOF
+		}
+		vr.err = err
+	}
+	return
+}
+
+func (vr *VerifyReader) Verify() error {
+	if vr.verified {
+		return nil
+	}
+	if vr.err == nil {
+		if vr.base.N > 0 {
+			return errors.New("early verify")
+		}
+	} else if vr.err != io.EOF {
+		return vr.err
+	}
+
+	if err := ioutil.EnsureEOF(vr.base.R); err != nil {
+		vr.err = ErrTrailingData
+		return vr.err
+	}
+	if !vr.verifier.Verified() {
+		vr.err = ErrMismatchedDigest
+		return vr.err
+	}
+
+	vr.verified = true
+	vr.err = io.EOF
+	return nil
+}
+
+func NewVerifyReader(r io.Reader, desc ocispec.Descriptor) *VerifyReader {
+	verifier := desc.Digest.Verifier()
+	lr := &io.LimitedReader{
+		R: io.TeeReader(r, verifier),
+		N: desc.Size,
+	}
+	return &VerifyReader{
+		base:     lr,
+		verifier: verifier,
+	}
+}
 
 // ReadAll safely reads the content described by the descriptor.
 // The read content is verified against the size and the digest.
